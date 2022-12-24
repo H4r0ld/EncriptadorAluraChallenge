e912af3cef40872a5b6b791c640ece02f488bf17
H4r0ld <harold@DESKTOP-LUP9I9Q>
2022-12-24 00:31:25 -0500

    logica de botones y encriptacion

--- main.js
+++ main.js
@@ -1,17 +1,79 @@
-
 const btnEncriptar = document.querySelector("#btnEncriptar");
 const btnDesencriptar = document.querySelector("#btnDesencriptar");
 const txaIngresoTexto = document.querySelector("#txaIngresoTexto");
+const pMostrarTexto = document.querySelector("#pMostrarTexto");
+const mensaje = document.querySelector(".mensaje");
+const imgTexto = document.querySelector(".imgTexto");
+const btnCopiar = document.querySelector(".button-mensaje");
 
-btnEncriptar.addEventListener("click",encriptar);
-btnDesencriptar.addEventListener("click",desencriptar);
+btnEncriptar.addEventListener("click", encriptar);
+btnDesencriptar.addEventListener("click", desencriptar);
+
+function encriptar() {
+  const p = txaIngresoTexto.value;
+  const isImgOff = imgTexto.classList.contains("inactive");
+
+  console.log(p);
+
+  if (p == "") {
+    //console.log("hola");
+
+    if (isImgOff) {
+      imgTexto.classList.remove("inactive");
+    }
+
+    mensaje.classList.add("inactive");
+    btnCopiar.classList.add("inactive");
 
 
-function encriptar(){
-    console.log(txaIngresoTexto.value)
+  } else {
+    const e = p.replaceAll("e", "enter");
+    const i = e.replaceAll("i", "imes");
+    const a = i.replaceAll("a", "ai");
+    const o = a.replaceAll("o", "ober");
+    const u = o.replaceAll("u", "ufat");
+
+    pMostrarTexto.innerHTML = u;
+    //console.log(u);
+
+    mensaje.classList.remove("inactive");
+
+    if (!isImgOff) {
+      imgTexto.classList.add("inactive");
+    }
+
     
+    btnCopiar.classList.remove("inactive");
+
+  }
 }
 
-function desencriptar(){
-    console.log("Desencriptando")
+function desencriptar() {
+  const p = txaIngresoTexto.value;
+  const isImgOff = imgTexto.classList.contains("inactive");
+
+  //console.log(p);
+  if (p == "") {
+
+    if (isImgOff) {
+      imgTexto.classList.remove("inactive");
+    }
+
+    mensaje.classList.add("inactive");
+    btnCopiar.classList.add("inactive");
+    
+  } else {
+    const e = p.replaceAll("enter", "e");
+    const i = e.replaceAll("imes", "i");
+    const a = i.replaceAll("ai", "a");
+    const o = a.replaceAll("ober", "o");
+    const u = o.replaceAll("ufat", "u");
+
+    pMostrarTexto.innerHTML = u;
+
+    mensaje.classList.remove("inactive");
+    if (!isImgOff) {
+      imgTexto.classList.add("inactive");
+    }
+  }
 }
